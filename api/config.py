9b43a4129d8defe8f875ce469db0cdaecaa12aae
FaganFeyzili <feyzilifeqan@gmail.com>
2026-06-10 12:07:27 +0000

    step3

--- api/config.py
+++ api/config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
-if not API_KEY:
-    raise ValueError("API_KEY not found in env file.")    
+if not API_KEY: 
+    raise ValueError("API_KEY not found in env file.")     
