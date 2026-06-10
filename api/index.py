4db1d70ce8839aaaed95d465752a8bcba86af9c5
FaganFeyzili <feyzilifeqan@gmail.com>
2026-06-10 11:34:17 +0000

    step2

--- api/index.py
+++ api/index.py
@@ -16,5 +16,4 @@ def get_news():
     if api_key and api_key != API_KEY:
         return jsonify({"error": "Invalid api key"})
 
-    return jsonify({"news": news})
-    
+    return jsonify({"news": news}) 
